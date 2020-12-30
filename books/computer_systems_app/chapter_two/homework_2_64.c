3287dc749de901f357602c1fa0556fdceaae24d4
Jonathon Belotti <jonathon@canva.com>
2020-12-30 13:26:11 +1100

    add homework problem 2.64 (not yet implemented)

---
+++ books/computer_systems_app/chapter_two/homework_2_64.c
@@ -0,0 +1,20 @@
+// 2.64 ♦
+//
+// Write code to implement the following function:
+//
+//     /* Return 1 when any odd bit of x equals 1; 0 otherwise.
+//        Assume w=32 */
+//     int any_odd_one(unsigned w);
+//
+// Your function should follow the bit-level integer coding rules (shown above),
+// except that you may assume that data type int has w = 32 bits.
+#include <stdio.h>
+
+/* Return 1 when any odd bit of x equals 1; 0 otherwise. Assume w=32 */
+int any_odd_one(unsigned w) {
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
