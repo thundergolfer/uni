1e15fa7572476e86b66a0d3a4f07a020fa6e2a32
Jonathon Belotti <jonathon@canva.com>
2021-06-24 22:35:15 +1000

    pytorch basics (fashion mnist)

---
+++ machine_learning/libraries/pytorch_basics/fashion_mnist.py
@@ -0,0 +1,146 @@
+"""
+Running through https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html.
+"""
+import logging
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda, Compose
+
+from typing import List
+
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-6s %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+def train(device, dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            logger.info(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+def test(device, dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    accuracy = 100 * correct
+    logger.info(f"Test Error: \n Accuracy: {accuracy:>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+def main():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    logger.info("Using {} device".format(device))
+    batch_size = 64
+
+    # Download training data from open datasets.
+    training_data = datasets.FashionMNIST(
+        root="data",
+        train=True,
+        download=True,
+        transform=ToTensor(),
+    )
+
+    # Download test data from open datasets.
+    test_data = datasets.FashionMNIST(
+        root="data",
+        train=False,
+        download=True,
+        transform=ToTensor(),
+    )
+    # Create data loaders.
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+    for X, y in test_dataloader:
+        logger.info("Shape of X [N, C, H, W]: ", X.shape)
+        logger.info("Shape of y: ", y.shape, y.dtype)
+        break
+
+    model = NeuralNetwork().to(device)
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+    epochs = 5
+    for t in range(epochs):
+        logger.info(f"Epoch {t+1}\n-------------------------------")
+        train(device, train_dataloader, model, loss_fn, optimizer)
+        test(device, test_dataloader, model, loss_fn)
+
+    logger.info("Done!")
+
+    torch.save(model.state_dict(), "model.pth")
+    logger.info("Saved PyTorch Model State to model.pth")
+
+    # Load the model straight back.
+    model = NeuralNetwork()
+    model.load_state_dict(torch.load("model.pth"))
+
+    classes: List[str] = {
+        "T-shirt/top",
+        "Trouser",
+        "Pullover",
+        "Dress",
+        "Coat",
+        "Sandal",
+        "Shirt",
+        "Sneaker",
+        "Bag",
+        "Ankle boot",
+    }
+    model.eval()
+    x, y = test_data[0][0], test_data[0][1]
+    with torch.no_grad():
+        pred = model(x)
+        predicted, actual = classes[pred[0].argmax(0)], classes[y]
+        logger.info(f'Predicted: "{predicted}", Actual: "{actual}"')
+
+
+if __name__ == "__main__":
+    main()
