8a91def33e622a8945a0ee33930430fd05efda41
Jonathon Belotti <jonathon@canva.com>
2021-07-18 19:24:25 +1000

    rust book: fix compile err

--- books/the_rust_programming_language/variables/src/main.rs
+++ books/the_rust_programming_language/variables/src/main.rs
@@ -8,8 +8,8 @@ fn main() {
 
     let tup: (i32, f64, u8) = (500, 6.4, 1);
 
-    let five_hundred = x.0;
-    let six_point_four = x.1;
-    let one = x.2;
+    let five_hundred = tup.0;
+    let six_point_four = tup.1;
+    let one = tup.2;
 }
 
