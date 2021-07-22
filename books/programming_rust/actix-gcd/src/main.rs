b374b5f65e889a8823e56530808b672ec0233f7b
Jonathon Belotti <jonathon@canva.com>
2021-07-22 23:12:36 +1000

    implementing arg parsing in mandelbrot

--- books/programming_rust/actix-gcd/src/main.rs
+++ books/programming_rust/actix-gcd/src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
 use serde::Deserialize;
+use std::str::FromStr;
 
 #[derive(Deserialize)]
 struct GcdParameters {
@@ -62,4 +63,35 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
         m = m % n;
     }
     n
+}
+
+/// Parse the string `s` as a coordinate pair, like `"400x600"` or `"1.0,0.5".
+///
+/// Specifically, `s` should have the form <left><sep><right>, where <sep> is the
+/// character given by the `separator` argument, and <left> and <right> are both
+/// strings that can be parsed by `T::from_str`. `separator` must be an ASCII character.
+///
+/// If `s` has the proper form, return `Some<(x, y)>`. If it doesn't parse correctly,
+/// return `None`.
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None // Match everything else, which captures all kinds of errors
+            }
+        }
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10,", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ',') None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10,20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
 }
