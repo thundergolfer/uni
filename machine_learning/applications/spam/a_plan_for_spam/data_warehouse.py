3f2807c7bdb2109c68c4db46728a0aa163bd39b3
Jonathon Belotti <jonathon@canva.com>
2021-09-14 22:15:15 +1000

    Sketch out data_warehouse.py module

--- machine_learning/applications/spam/a_plan_for_spam/data_warehouse.py
+++ machine_learning/applications/spam/a_plan_for_spam/data_warehouse.py
@@ -2,6 +2,36 @@
 The data warehouse module exists to process the structured event logging
 by the other system components and run offline analysis.
 """
+from typing import Generator, NamedTuple
+
+
+class EmailSpamDatasetRow(NamedTuple):
+    text: str
+    spam: bool
+    # send_datetime: datetime.datetime
+
+
+def email_spam_dataset(
+    *,
+    start: int,
+    end: int,
+) -> Generator[EmailSpamDatasetRow, None, None]:
+    """
+    :param start: epoch timestamp as integer
+    :param end: epoch timestamp as integer
+    :return: a dataset for emails processed between `from` and `to`.
+    """
+    # Read EmailProcessedEvents
+
+    # Read EmailMarkedSpamEvents
+
+    # Combine into single dataset
+
+    yield EmailSpamDatasetRow(
+        text="",
+        spam=False,
+    )
+
 
 if __name__ == "__main__":
     pass
