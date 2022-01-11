31cedfc124d2e9cf497846e0993c5cdb496f5727
Jonathon Belotti <jonathon@canva.com>
2022-01-11 22:12:08 +1100

    a plan for spam: rename test classifier

--- machine_learning/applications/spam/a_plan_for_spam/model_trainer_integration_test.py
+++ machine_learning/applications/spam/a_plan_for_spam/model_trainer_integration_test.py
@@ -7,32 +7,32 @@ from model_trainer import Email
 from model_trainer import Prediction
 
 
-def fake_classifier(email: Email) -> Prediction:
+def dummy_classifier(email: Email) -> Prediction:
     return 0.86
 
 
 def test_load_classifier_success(tmp_path):
     tmp_classifier_dest_path = model_trainer.store_classifier(
-        classifier_func=fake_classifier,
+        classifier_func=dummy_classifier,
         classifier_destination_root=tmp_path,
         current_git_commit_hash="TEST-NOT-REALLY-A-COMMIT-HASH",
     )
 
-    loaded_fake_classifier = model_trainer.load_serialized_classifier(
+    loaded_dummy_classifier = model_trainer.load_serialized_classifier(
         classifier_sha256_hash=tmp_classifier_dest_path.name,  # NOTE: Implementation detail leak
         classifier_destination_root=tmp_path,
     )
     test_email = "test email: doesn't matter what contents"
-    assert fake_classifier(email=test_email) == loaded_fake_classifier(email=test_email)
+    assert dummy_classifier(email=test_email) == loaded_dummy_classifier(email=test_email)
 
 
 def test_load_classifier_corrupted_data(tmp_path):
-    fake_classifier_b: bytes = model_trainer.serialize_classifier(fake_classifier)
+    dummy_classifier_b: bytes = model_trainer.serialize_classifier(dummy_classifier)
     # intentionally write classifier to non-content-addressed location. bogus path.
     bogus_hash = "bogus_pocus"
     bogus_path = tmp_path / bogus_hash
     with open(bogus_path, "wb") as f:
-        f.write(fake_classifier_b)
+        f.write(dummy_classifier_b)
 
     with pytest.raises(ValueError):
         _ = model_trainer.load_serialized_classifier(
