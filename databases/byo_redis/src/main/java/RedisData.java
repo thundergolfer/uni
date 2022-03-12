048c7678a1f050895251a572f82213d2f02293c9
Jonathon Belotti <jonathon@canva.com>
2022-03-12 23:00:53 +1100

    byo redis: add new class to clean up scan->command transition

---
+++ databases/byo_redis/src/main/java/RedisData.java
@@ -0,0 +1,48 @@
+import java.util.List;
+
+enum RedisDataType {
+    SIMPLE_STRING,
+    BULK_STRING, // TODO(Jonathon): Stop handling all strings the same.
+    INTEGER,
+    ARRAY,
+}
+
+public class RedisData {
+    private RedisDataType type;
+    private String strValue;
+    private Integer intValue;
+    private List<RedisData> arrValue;
+
+    public RedisData(String val) {
+        this.type = RedisDataType.SIMPLE_STRING;
+        this.strValue = val;
+        this.intValue = null;
+        this.arrValue = null;
+    }
+
+    public RedisData(int val) {
+        this.type = RedisDataType.INTEGER;
+        this.strValue = null;
+        this.intValue = val;
+        this.arrValue = null;
+    }
+
+    public RedisData(List<RedisData> val) {
+        this.type = RedisDataType.ARRAY;
+        this.strValue = null;
+        this.intValue = null;
+        this.arrValue = val;
+    }
+
+    public RedisDataType getType() {
+        return this.type;
+    }
+
+    public static RedisData from(List<Token> tokens) {
+        return null; // TODO
+    }
+
+    public static RedisData from(RedisSerializationProtocol.Array RESPArr) {
+        return null; // TODO
+    }
+}
