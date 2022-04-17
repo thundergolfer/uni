d7bc029542183e315d18976109a1b83f5b8f6dc3
Jonathon Belotti <jonathon@canva.com>
2022-04-17 12:43:52 +1000

    crafting interpreters: clox: 14.4

--- books/crafting_interpreters/clox/lox/main.c
+++ books/crafting_interpreters/clox/lox/main.c
@@ -1,10 +1,13 @@
 #include "common.h"
 #include "chunk.h"
+#include "debug.h"
 
 int main(int argc, const char* argv[]) {
     Chunk chunk;
     initChunk(&chunk);
     writeChunk(&chunk, OP_RETURN);
+
+    disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
     return 0;
 }
