b9800f91fcceca2c0ac8bb8b107221a7180b1e58
Jonathon Belotti <jonathon@canva.com>
2022-04-17 17:05:59 +1000

    crafting interpreters: clox: 15.1, 15.2

--- books/crafting_interpreters/clox/lox/chunk.c
+++ books/crafting_interpreters/clox/lox/chunk.c
@@ -7,10 +7,12 @@ void initChunk(Chunk* chunk) {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+    initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk* chunk) {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
 
@@ -23,4 +25,9 @@ void writeChunk(Chunk* chunk, uint8_t byte) {
 
     chunk->code[chunk->count] = byte;
     chunk->count++;
+}
+
+int addConstant(Chunk* chunk, Value value) {
+    writeValueArray(&chunk->constants, value);
+    return chunk->constants.count - 1;
 }
--- books/crafting_interpreters/clox/lox/chunk.h
+++ books/crafting_interpreters/clox/lox/chunk.h
@@ -2,6 +2,7 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
     OP_RETURN,
@@ -11,10 +12,12 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte);
+int addConstant(Chunk* chunk, Value value);
 
 #endif
