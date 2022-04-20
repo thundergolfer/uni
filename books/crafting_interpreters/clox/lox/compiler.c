d4c965e48f93a2679583c2c35ff0ee5af9e30b0b
Jonathon Belotti <jonathon@canva.com>
2022-04-20 20:52:17 +1000

    crafting interpreters: clox: 16.2 -> end of 16

--- books/crafting_interpreters/clox/lox/compiler.c
+++ books/crafting_interpreters/clox/lox/compiler.c
@@ -6,4 +6,16 @@
 
 void compile(const char* source) {
     initScanner(source);
+    int line = -1;
+    for (;;) {
+        Token token = scanToken();
+        if (token.line != line) {
+            printf("%4d ", token.line);
+            line = token.line;
+        } else {
+            printf("    | ");
+        }
+        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+        if (token.type == TOKEN_EOF) break;
+    }
 }
