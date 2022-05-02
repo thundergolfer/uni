4d2832cb5cf7b7484dbe39cbc1cb727178fdbf7b
Jonathon Belotti <jonathon@canva.com>
2022-05-02 22:32:46 +1000

    crafting interpreters: clox: fix bug from mistype that causes segfault

--- books/crafting_interpreters/clox/lox/object.c
+++ books/crafting_interpreters/clox/lox/object.c
@@ -22,7 +22,7 @@ static Obj* allocateObject(size_t size, ObjType type) {
 ObjClosure* newClosure(ObjFunction* function) {
     ObjUpvalue** upvalues = ALLOCATE(ObjUpvalue*,
                                      function->upvalueCount);
-    for (int i = 0; function->upvalueCount; i++) {
+    for (int i = 0; i < function->upvalueCount; i++) {
         upvalues[i] = NULL;
     }
     ObjClosure* closure = ALLOCATE_OBJ(ObjClosure, OBJ_CLOSURE);
