38d4bc3153f9104afde4b0cd8861bf80831a4f42
Jonathon Belotti <jonathon@canva.com>
2022-05-08 14:47:08 +1000

    crafting interpreters: clox: 30.2 (optimization chaptr)

--- books/crafting_interpreters/clox/lox/table.c
+++ books/crafting_interpreters/clox/lox/table.c
@@ -18,7 +18,7 @@ void freeTable(Table* table) {
 }
 
 static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
-    uint32_t index = key->hash % capacity;
+    uint32_t index = key->hash & (capacity - 1);
     Entry* tombstone = NULL;
     for (;;) {
         Entry* entry = &entries[index];
@@ -33,7 +33,7 @@ static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
             return entry;
         }
 
-        index = (index + 1) % capacity;
+        index = (index + 1) & (capacity - 1);
     }
 }
 
@@ -111,7 +111,7 @@ ObjString* tableFindString(Table* table, const char* chars,
                            int length, uint32_t hash) {
     if (table->count == 0) return NULL;
 
-    uint32_t index = hash % table->capacity;
+    uint32_t index = hash & (table->capacity - 1);
     for (;;) {
         Entry* entry = &table->entries[index];
         if (entry->key == NULL) {
@@ -125,7 +125,7 @@ ObjString* tableFindString(Table* table, const char* chars,
             return entry->key;
         }
 
-        index = (index + 1) % table->capacity;
+        index = (index + 1) & (table->capacity - 1);
     }
 }
 
