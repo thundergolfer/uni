78571e81c88233fb5502abd6e8ae77ff2a19ffb2
Jonathon Belotti <jonathon.i.belotti@gmail.com>
2024-05-19 11:17:37 +0200

    refactor code into lib.rs for testing

--- operating_systems/linux/strace_rs/src/lib.rs
+++ operating_systems/linux/strace_rs/src/lib.rs
@@ -4,4 +4,63 @@ extern crate num_derive;
 #[macro_use]
 extern crate bitflags;
 
+use libc;
+use std::ffi::CString;
+use std::io::Error;
+use std::ptr;
+
+use tracing::{debug, error};
 pub mod ptrace;
+
+unsafe fn do_child<T>(args: T) -> i32
+where T: IntoIterator<Item = String> {
+    let cstrings: Vec<CString> = args.into_iter()
+        .map(|arg| CString::new(arg.as_str()).expect("CString::new failed"))
+        .collect();
+
+    let child_prog = cstrings.get(0).unwrap().clone();
+    debug!(?child_prog, "starting child");
+    let child_prog = child_prog.into_raw();
+    let mut c_pointers: Vec<*const libc::c_char> = cstrings
+        .iter()
+        .map(|cstr| cstr.as_ptr())
+        .collect();
+    // Ensure null termination for C-style argv array
+    c_pointers.push(ptr::null());
+
+    let argv: *const *const libc::c_char = c_pointers.as_ptr();
+
+    let mut envp: Vec<*const libc::c_char> = std::env::vars()
+            .into_iter()
+            .map(|(k,v)| CString::new(format!("{k}={v}")).expect("cannot fail"))
+            .map(|cstr| cstr.as_ptr())
+            .collect();
+    envp.push(ptr::null());
+    let envp: *const *const libc::c_char = envp.as_ptr();
+    libc::execve(child_prog, argv, envp);
+
+    // If execution continued to here there was an error.
+    let errno: i32 = Error::last_os_error().raw_os_error().unwrap();
+    error!("errno = {}", errno);
+    errno
+}
+
+fn do_trace(child: i32) -> i32 {
+    debug!(%child, "starting trace of child");
+    let _ = child;
+    std::thread::sleep(std::time::Duration::from_secs(2));
+    0
+}
+
+pub unsafe fn trace_command<T>(args: T) -> i32 
+where T: IntoIterator<Item = String> {
+    unsafe {
+        let child = libc::fork();
+        debug!(?child, "after fork");
+        if child == 0 {
+            do_child(args)
+        } else {
+            do_trace(child)
+        }
+    }
+}
--- operating_systems/linux/strace_rs/src/main.rs
+++ operating_systems/linux/strace_rs/src/main.rs
@@ -1,51 +1,9 @@
-use libc;
-use std::ffi::CString;
-use std::io::Error;
 use std::process;
-use std::ptr;
 
-use tracing::{debug, info, warn, error, Level};
+use tracing::Level;
 use tracing_subscriber;
 
-unsafe fn do_child<T>(args: T) -> i32
-where T: IntoIterator<Item = String> {
-    let cstrings: Vec<CString> = args.into_iter()
-        .map(|arg| CString::new(arg.as_str()).expect("CString::new failed"))
-        .collect();
-
-    let child_prog = cstrings.get(0).unwrap().clone();
-    debug!(?child_prog, "starting child");
-    let child_prog = child_prog.into_raw();
-    let mut c_pointers: Vec<*const libc::c_char> = cstrings
-        .iter()
-        .map(|cstr| cstr.as_ptr())
-        .collect();
-    // Ensure null termination for C-style argv array
-    c_pointers.push(ptr::null());
-
-    let argv: *const *const libc::c_char = c_pointers.as_ptr();
-
-    let mut envp: Vec<*const libc::c_char> = std::env::vars()
-            .into_iter()
-            .map(|(k,v)| CString::new(format!("{k}={v}")).expect("cannot fail"))
-            .map(|cstr| cstr.as_ptr())
-            .collect();
-    envp.push(ptr::null());
-    let envp: *const *const libc::c_char = envp.as_ptr();
-    libc::execve(child_prog, argv, envp);
-
-    // If execution continued to here there was an error.
-    let errno: i32 = Error::last_os_error().raw_os_error().unwrap();
-    error!("errno = {}", errno);
-    errno
-}
-
-fn do_trace(child: i32) -> i32 {
-    debug!(%child, "starting trace of child");
-    let _ = child;
-    std::thread::sleep(std::time::Duration::from_secs(2));
-    0
-}
+use strace_rs::trace_command;
 
 fn main() {
     let mut args = std::env::args();
@@ -58,30 +16,17 @@ fn main() {
     } else {
         let _ = args.next().expect("always exists");
     }
-    
-    let argv = args.into_iter();
 
     tracing_subscriber::fmt()
         .with_max_level(Level::TRACE)
         .init();
-
-    info!("This is an info message.");
-    warn!("This is a warning message.");
-    error!("This is an error message.");
-    debug!("This is a debug message.");
 
     // We’ll start with the entry point. We check that we were passed a command,
     // and then we fork() to create two processes –
     // one to execute the program to be traced, and
     // the other to trace it.
     let exit_code = unsafe {
-        let child = libc::fork();
-        debug!(?child, "after fork");
-        if child == 0 {
-            do_child(argv)
-        } else {
-            do_trace(child)
-        }
+        trace_command(args.into_iter())
     };
     process::exit(exit_code);
 }
